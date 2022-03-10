a060e6dfcfb64a3986ac952680a1690985943aa7
Phacharapol18 <98699809+Phacharapol18@users.noreply.github.com>
2022-03-10 01:22:53 -0800

    gameFile

---
+++ index.js
@@ -0,0 +1,18 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let image1 = "images/dice" + randomNumber1 + ".png";
+let image2 = "images/dice" + randomNumber2 + ".png";
+
+
+document.querySelector(".dice .img1").setAttribute("src", image1);
+document.querySelector(".dice .img2").setAttribute("src", image2);
+
+
+if (randomNumber1 === randomNumber2){
+  document.querySelector('h1').innerHTML = "Draw!!";
+}else if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = "🔥Player 1 Win!";
+}else if (randomNumber1 < randomNumber2) {
+  document.querySelector('h1').innerHTML = "🔥Player 2 Win!";
+}
